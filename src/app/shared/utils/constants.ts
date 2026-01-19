a1c0cf18af853dcc8e4b22cd9a4d209f1f0a3d4d
Hashmat Ali <hashmatali1431@gmail.com>
2026-01-19 23:13:19 +0500

    changed the links and icon

--- src/app/shared/utils/constants.ts
+++ src/app/shared/utils/constants.ts
@@ -1,4 +1,4 @@
 export const APP_NAME = 'Gateway';
 export const APP_DESCRIPTION = 'Professional Call Center Solutions & Training Institute';
-export const CONTACT_PHONE = '+92 (302) 102-5200';
+export const CONTACT_PHONE = '+92 308 841 5668';
 export const CONTACT_EMAIL = 'info@gateway.edu';
